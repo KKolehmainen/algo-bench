3a4802f2af8f452e57892dc71b57bde74ddbd4aa
Kauri Kolehmainen <kauri.kolehmainen@helsinki.fi>
2026-05-21 11:32:58 +0300

    add login and session functionality

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect
-from werkzeug.security import generate_password_hash
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import db
-
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login_page")
+def login_page():
+    return render_template("login_page.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    try:
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        password_hash = db.query(sql, [username])[0][0]
+    except:
+        return "VIRHE: väärä käyttäjätunnus tai salasana" 
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/")
+    else:
+        return "VIRHE: väärä käyttäjätunnus tai salasana"
+    
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 @app.route("/register")
 def register():
