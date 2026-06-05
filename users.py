f4eb5983c4d64121d182bd33c754e5d06195e6b4
Kauri Kolehmainen <kauri.kolehmainen@helsinki.fi>
2026-06-05 13:48:43 +0300

    add users module for performing user-related actions and querys

---
+++ users.py
@@ -0,0 +1,10 @@
+import db
+
+def get_user(user_id):
+    sql = "SELECT username FROM users WHERE id = ?"
+    result = db.query(sql, [user_id])
+    return result[0] if result else None
+
+def get_algorithms_by_user(username):
+    sql = "SELECT name FROM algorithms WHERE username = ?"
+    return db.query(sql, [username])
