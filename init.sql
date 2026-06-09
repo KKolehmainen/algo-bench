276fd006be4224db2118b8515d5153687d04a89d
Kauri Kolehmainen <kauri.kolehmainen@helsinki.fi>
2026-06-09 15:09:36 +0300

    add file to initialize classes table

---
+++ init.sql
@@ -0,0 +1,9 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'Python');
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'C');
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'C++');
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'Java');
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'Fortran');
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'Julia');
+INSERT INTO classes (title, value) VALUES ('Ohjelmointikieli', 'Haskell');
